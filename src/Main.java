a2bb4e52f6e164760ab62662a4dbece1cc1e1451
tamura aoi <a.tamura.sys23@morijyobi.ac.jp>
2024-07-19 11:34:33 +0900

    スプリント3をした close #5 , close #16 , close #17

--- src/Main.java
+++ src/Main.java
@@ -91,9 +91,10 @@ public class Main {
         }
 
 
-
+        com.createRandomHand();
         //以降にテストコードを追加して検証を行う
-
+//        System.out.println("ランダムなジャンケンの手をCPUにセットする");
+//        System.out.println(com.getHand().getHandName());
 
 
     }
